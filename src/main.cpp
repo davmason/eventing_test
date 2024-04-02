5881ec22d06e57e749475ce3d290213b23a90f8e
David Mason <davmason@microsoft.com>
2024-04-01 23:41:52 -0700

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,60 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
+#include <eventheader/TraceLoggingProvider.h>
 
-void main()
+#include <inttypes.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+TRACELOGGING_DEFINE_PROVIDER(
+    MyProvider,
+    "MyProviderName",
+    // {b7aa4d18-240c-5f41-5852-817dbf477472}
+    (0xb7aa4d18, 0x240c, 0x5f41, 0x58, 0x52, 0x81, 0x7d, 0xbf, 0x47, 0x74, 0x72));
+
+int main()
 {
-	
+    int err = 0;
+
+    printf("\n");
+
+    TraceLoggingRegister(MyProvider);
+
+    event_level const event1_level = event_level_information;
+    uint64_t const event1_keyword = 0x1;
+
+    printf("Waiting for provider to be enabled.\n");
+    while (!TraceLoggingProviderEnabled(MyProvider, event1_level, event1_keyword))
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    printf("Provider enabled.\n");
+
+    const int event_count = 500000;
+    const auto start = std::chrono::steady_clock::now();
+    for (unsigned iteration = 1; iteration <= event_count; iteration += 1)
+    {
+        TraceLoggingWrite(
+            MyProvider,                               // Provider to use for the event.
+            "SimpleEvent",                                 // Event name.
+            TraceLoggingLevel(event1_level),          // Event severity level.
+            TraceLoggingKeyword(event1_keyword),      // Event category bits.
+            TraceLoggingUInt32(iteration));           // uint32 field named "iteration".
+    }
+
+    const auto end = std::chrono::steady_clock::now();
+    const auto diff = end - start;
+
+    const double seconds = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() / 1000.0;
+
+    printf("Time to fire %d simple events: %f\n", event_count, seconds);
+
+    TraceLoggingUnregister(MyProvider);
+    return err;
 }
