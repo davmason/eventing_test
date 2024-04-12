43e36319e859243919d2e353a386ebc6d2861315
David Mason <davmason@microsoft.com>
2024-04-11 23:13:49 -0700

    WIP

--- src/event_main.cpp
+++ src/event_main.cpp
@@ -63,47 +63,47 @@ void time_it(std::function<void()> work, std::string description)
 
 int main()
 {
-    int data_fd, simple_write, big_write;
-    __u32 count = 0;
+    // int data_fd, simple_write, big_write;
+    // __u32 count = 0;
 
-    data_fd = open(data_file, O_RDWR);
+    // data_fd = open(data_file, O_RDWR);
 
-    if (event_reg(data_fd, "test u32 iteration", &simple_write, &simple_enabled) == -1
-        || event_reg(data_fd, "test2 char[1000] array", &big_write, &big_enabled) == -1)
-    {
-        printf("error user_events: %s\n", strerror(errno));
-        return errno;
-    }
-
-    // int err = 0;
-
-    // printf("\n");
-
-    // err = TraceLoggingRegister(MyProvider);
-    // if (err != 0)
+    // if (event_reg(data_fd, "test u32 iteration", &simple_write, &simple_enabled) == -1
+    //     || event_reg(data_fd, "test2 char[1000] array", &big_write, &big_enabled) == -1)
     // {
-    //     printf("Error registering MyProvider err=%d\n", err);
-    //     return err;
+    //     printf("error user_events: %s\n", strerror(errno));
+    //     return errno;
     // }
 
-    // event_level const event1_level = event_level_information;
-    // uint64_t const event1_keyword = 0x1;
+    int err = 0;
 
-    // TraceLoggingWrite(
-    //     MyProvider,                               // Provider to use for the event.
-    //     "SimpleEvent",                                 // Event name.
-    //     TraceLoggingLevel(event1_level),          // Event severity level.
-    //     TraceLoggingKeyword(event1_keyword),      // Event category bits.
-    //     TraceLoggingUInt32(0));           // uint32 field named "iteration".
+    printf("\n");
+
+    err = TraceLoggingRegister(MyProvider);
+    if (err != 0)
+    {
+        printf("Error registering MyProvider err=%d\n", err);
+        return err;
+    }
+
+    event_level const event1_level = event_level_information;
+    uint64_t const event1_keyword = 0x1;
+
+    TraceLoggingWrite(
+        MyProvider,                               // Provider to use for the event.
+        "SimpleEvent",                                 // Event name.
+        TraceLoggingLevel(event1_level),          // Event severity level.
+        TraceLoggingKeyword(event1_keyword),      // Event category bits.
+        TraceLoggingUInt32(0));           // uint32 field named "iteration".
 
     printf("Waiting for event to be enabled.\n");
-    // while (!TraceLoggingProviderEnabled(MyProvider, event1_level, event1_keyword))
-    while (!simple_enabled)
+    while (!TraceLoggingProviderEnabled(MyProvider, event1_level, event1_keyword))
+    // while (!simple_enabled)
     {
-        // printf("MyProviderName_L4K1 Event1 status=%x\n",
-        //     TraceLoggingProviderEnabled(MyProvider, event1_level, event1_keyword));
-        printf("user_events simple_enabled=%d\n", simple_enabled);
-        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+        printf("MyProviderName_L4K1 Event1 status=%x\n",
+            TraceLoggingProviderEnabled(MyProvider, event1_level, event1_keyword));
+        // printf("user_events simple_enabled=%d\n", simple_enabled);
+        // std::this_thread::sleep_for(std::chrono::milliseconds(5000));
     }
 
     printf("Event enabled.\n");
@@ -114,55 +114,55 @@ int main()
         struct iovec io[2];
         for (unsigned iteration = 1; iteration <= event_count; iteration += 1)
         {
-            io[0].iov_base = &simple_write;
-            io[0].iov_len = sizeof(simple_write);
-            io[1].iov_base = &iteration;
-            io[1].iov_len = sizeof(iteration);
+            // io[0].iov_base = &simple_write;
+            // io[0].iov_len = sizeof(simple_write);
+            // io[1].iov_base = &iteration;
+            // io[1].iov_len = sizeof(iteration);
 
-            if (writev(data_fd, (const struct iovec *)io, 2) == -1)
-            {
-                printf("Error writing event %s\n", strerror(errno));
-                return;
-            }
-            // TraceLoggingWrite(
-            //     MyProvider,                               // Provider to use for the event.
-            //     "SimpleEvent",                                 // Event name.
-            //     TraceLoggingLevel(event1_level),          // Event severity level.
-            //     TraceLoggingKeyword(event1_keyword),      // Event category bits.
-            //     TraceLoggingUInt32(iteration));           // uint32 field named "iteration".
+            // if (writev(data_fd, (const struct iovec *)io, 2) == -1)
+            // {
+            //     printf("Error writing event %s\n", strerror(errno));
+            //     return;
+            // }
+            TraceLoggingWrite(
+                MyProvider,                               // Provider to use for the event.
+                "SimpleEvent",                                 // Event name.
+                TraceLoggingLevel(event1_level),          // Event severity level.
+                TraceLoggingKeyword(event1_keyword),      // Event category bits.
+                TraceLoggingUInt32(iteration));           // uint32 field named "iteration".
         }
     };
     time_it(simple_work, "Firing 500,000 simple events");
 
-    printf("What?\n");
-    std::function<void()> big_work = [&]()
-    {
-        const int event_count = 500000;
-        struct iovec io[2];
-        char data_buffer[1000];
-        memset(data_buffer, 11, 1000);
+    // printf("What?\n");
+    // std::function<void()> big_work = [&]()
+    // {
+    //     const int event_count = 500000;
+    //     struct iovec io[2];
+    //     char data_buffer[1000];
+    //     memset(data_buffer, 11, 1000);
 
-        for (unsigned iteration = 1; iteration <= event_count; iteration += 1)
-        {
-            io[0].iov_base = &big_write;
-            io[0].iov_len = sizeof(big_write);
-            io[1].iov_base = data_buffer;
-            io[1].iov_len = 1000;
+    //     for (unsigned iteration = 1; iteration <= event_count; iteration += 1)
+    //     {
+    //         io[0].iov_base = &big_write;
+    //         io[0].iov_len = sizeof(big_write);
+    //         io[1].iov_base = data_buffer;
+    //         io[1].iov_len = 1000;
 
-            if (writev(data_fd, (const struct iovec *)io, 2) == -1)
-            {
-                printf("Error writing event %s\n", strerror(errno));
-                return;
-            }
-            // TraceLoggingWrite(
-            //     MyProvider,                               // Provider to use for the event.
-            //     "SimpleEvent",                                 // Event name.
-            //     TraceLoggingLevel(event1_level),          // Event severity level.
-            //     TraceLoggingKeyword(event1_keyword),      // Event category bits.
-            //     TraceLoggingUInt32(iteration));           // uint32 field named "iteration".
-        }
-    };
-    time_it(big_work, "Firing 500,000 big events");
+    //         if (writev(data_fd, (const struct iovec *)io, 2) == -1)
+    //         {
+    //             printf("Error writing event %s\n", strerror(errno));
+    //             return;
+    //         }
+    //         // TraceLoggingWrite(
+    //         //     MyProvider,                               // Provider to use for the event.
+    //         //     "SimpleEvent",                                 // Event name.
+    //         //     TraceLoggingLevel(event1_level),          // Event severity level.
+    //         //     TraceLoggingKeyword(event1_keyword),      // Event category bits.
+    //         //     TraceLoggingUInt32(iteration));           // uint32 field named "iteration".
+    //     }
+    // };
+    // time_it(big_work, "Firing 500,000 big events");
 
     // TraceLoggingUnregister(MyProvider);
     // return err;
